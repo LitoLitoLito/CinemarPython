bdbc7bbffbe7a39118e2c8a7f3432eff21a6b830
Julio <saltagaceta@yahoo.com.ar>
2022-12-01 20:31:12 -0300

    1 diciembre

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from os import system
+
 from funciones import pedirNumeroEntero, agregarCliente, conexionDB
 
 from funcionesPeliculas import crearFunciones
@@ -10,26 +12,33 @@ from usuarios import agregarDB, crearUsuario, verUsuarios
 
 from funcionesPeliculas import agregarDbFunciones
 
+
+
 menuInicio()
 salir = False
 opcion = 0
  
-while not salir: 
+while not salir:
+     
     print ("Elige una opcion") 
     opcion = pedirNumeroEntero() 
     if opcion == 1:
+        system("cls")
         email1 = agregarCliente()
         us = crearUsuario(email1)
         conn = conexionDB()
         agregarDB(conn, us.idusuario, us.clave, us.nombre, us.apellido, us.estado, us.fechanacimiento, us.celular)
         menuInicio()
     elif opcion == 2:
+        system("cls")
         print ("Opcion 2")
         menuInicio()
     elif opcion == 3:
+        system("cls")
         print("Ver CARTELERA 3")
         menuInicio()
     elif opcion == 4:
+        system("cls")
         administrador()
         acceso = int(input())
         if acceso==1234:
@@ -40,39 +49,48 @@ while not salir:
                 print ("Elige una opcion") 
                 opcion = pedirNumeroEntero() 
                 if opcion == 1:
+                    system("cls")
                     print('1.- Crear Salas')
                     administrar()
                 elif opcion == 2:
+                    system("cls")
                     us1 = crearFunciones()
                     conn = conexionDB()
                     agregarDbFunciones(conn, us1.Idfuncion, us1.Idsala, us1.Idpelicula, us1.Horario, us1.Fecha, us1.Estado)
                     administrar()
                 elif opcion == 3:
+                    system("cls")
                     peliculas()
                     us2 = agregarPelicula()
                     conn = conexionDB()
                     agregarDbPelicula(conn, us2.Titulo, us2.Director, us2.Descripcion, us2.Genero, us2.Duracion)
                     administrar()
                 elif opcion == 4:
-                        print('4.- Eliminar Películas')
-                        administrar()        
+                    system("cls")
+                    print('4.- Eliminar Películas')
+                    administrar()        
                 elif opcion == 5:
+                    system("cls")
                     conn = conexionDB()
                     verPeliculas(conn)
                     administrar()
                 elif opcion == 6:
+                    system("cls")
                     conn = conexionDB()
                     verUsuarios(conn)
                     administrar() 
                 elif opcion == 7:
+                    system("cls")
                     salir1 = True
                 else:
                     print ("Introduce un numero entre 1 y 7")
                             
         else:
+            system("cls")
             print("Acceso DENEGADO")
         menuInicio()
     elif opcion == 5:
+        system("cls")
         salir = True
     else:
         print ("Introduce un numero entre 1 y 5")
---
+++ registrarse.py
@@ -0,0 +1,110 @@
+from os import system
+import tkinter as tk
+import tkinter.font as tkFont
+
+from funciones import agregarCliente, conexionDB
+from usuarios import agregarDB, crearUsuario
+from vistas import administrador, administrar
+
+class App:
+    def __init__(self, root):
+        #setting title
+        root.title("Menu Inicio")
+        #setting window size
+        width=328
+        height=457
+        screenwidth = root.winfo_screenwidth()
+        screenheight = root.winfo_screenheight()
+        alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
+        root.geometry(alignstr)
+        root.resizable(width=False, height=False)
+
+        GMessage_334=tk.Message(root)
+        ft = tkFont.Font(family='Times',size=10)
+        GMessage_334["font"] = ft
+        GMessage_334["fg"] = "#333333"
+        GMessage_334["justify"] = "center"
+        GMessage_334["text"] = "INICIO"
+        GMessage_334.place(x=0,y=30,width=320,height=30)
+
+        GButton_365=tk.Button(root)
+        GButton_365["bg"] = "#cc0000"
+        ft = tkFont.Font(family='Times',size=11)
+        GButton_365["font"] = ft
+        GButton_365["fg"] = "#ffffff"
+        GButton_365["justify"] = "center"
+        GButton_365["text"] = "Registrarse"
+        GButton_365.place(x=10,y=70,width=305,height=30)
+        GButton_365["command"] = self.registrar
+        
+        GButton_365=tk.Button(root)
+        GButton_365["bg"] = "#cc0000"
+        ft = tkFont.Font(family='Times',size=11)
+        GButton_365["font"] = ft
+        GButton_365["fg"] = "#ffffff"
+        GButton_365["justify"] = "center"
+        GButton_365["text"] = "Reservar Función"
+        GButton_365.place(x=10,y=105,width=305,height=30)
+        GButton_365["command"] = self.reservarFuncion
+        
+        GButton_365=tk.Button(root)
+        GButton_365["bg"] = "#cc0000"
+        ft = tkFont.Font(family='Times',size=11)
+        GButton_365["font"] = ft
+        GButton_365["fg"] = "#ffffff"
+        GButton_365["justify"] = "center"
+        GButton_365["text"] = "Ver Funciones"
+        GButton_365.place(x=10,y=140,width=305,height=30)
+        GButton_365["command"] = self.verFunciones
+        
+        GButton_365=tk.Button(root)
+        GButton_365["bg"] = "#cc0000"
+        ft = tkFont.Font(family='Times',size=11)
+        GButton_365["font"] = ft
+        GButton_365["fg"] = "#ffffff"
+        GButton_365["justify"] = "center"
+        GButton_365["text"] = "Administrar"
+        GButton_365.place(x=10,y=175,width=305,height=30)
+        GButton_365["command"] = self.administrador
+        
+        GButton_365=tk.Button(root)
+        GButton_365["bg"] = "#cc0000"
+        ft = tkFont.Font(family='Times',size=11)
+        GButton_365["font"] = ft
+        GButton_365["fg"] = "#ffffff"
+        GButton_365["justify"] = "center"
+        GButton_365["text"] = "Salir"
+        GButton_365.place(x=10,y=210,width=305,height=30)
+        GButton_365["command"] = self.quit
+        
+    def quit(self):
+        root.destroy()
+
+    def registrar(self):
+        print("command")
+        email1 = agregarCliente()
+        us = crearUsuario(email1)
+        conn = conexionDB()
+        agregarDB(conn, us.idusuario, us.clave, us.nombre, us.apellido, us.estado, us.fechanacimiento, us.celular)
+        print("command EXITOSO")
+    
+    def reservarFuncion(self):
+        pass
+    
+    def verFunciones(self):
+        pass
+    
+    def administrador(self):
+        system("cls")
+        administrador()
+        acceso = int(input())
+        if acceso==1234:
+            print("Acceso PERMITIDO")
+            administrar()
+            salir1=False
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = App(root)
+    root.mainloop()
